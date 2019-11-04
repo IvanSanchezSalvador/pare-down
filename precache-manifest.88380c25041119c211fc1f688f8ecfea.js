27e13633799fa3321d3333dc1d1f88c2aa681435
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2019-11-04 18:09:00 +0100

    Updates

---
+++ precache-manifest.88380c25041119c211fc1f688f8ecfea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06da067c327a8f783ee0d49b662ce5da",
+    "url": "/pare-down/index.html"
+  },
+  {
+    "revision": "df0e1c0eb33403769c4a",
+    "url": "/pare-down/static/css/main.f4d58520.chunk.css"
+  },
+  {
+    "revision": "c1d0e2c04cb28970ff0c",
+    "url": "/pare-down/static/js/2.72760a56.chunk.js"
+  },
+  {
+    "revision": "df0e1c0eb33403769c4a",
+    "url": "/pare-down/static/js/main.9fc81ab0.chunk.js"
+  },
+  {
+    "revision": "0ca0e7eb6e1146cdeac7",
+    "url": "/pare-down/static/js/runtime-main.47fe04d8.js"
+  }
+]);
